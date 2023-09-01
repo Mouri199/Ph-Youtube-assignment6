4535c82f9f52a3c25f8d07fbb1b29a537e1a2179
Mouri199 <138799900+Mouri199@users.noreply.github.com>
2023-09-01 23:54:05 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,110 @@
+function openBlog() {
+    window.open('blog-answer.html', '_blank')
+}
+
+
+const loadCategory = async () => {
+    const res = await fetch("https://openapi.programming-hero.com/api/videos/categories");
+    const data = await res.json();
+
+
+    const buttonContainer = document.getElementById('button-container');
+    const viewData = data.data;
+
+    // console.log(viewData);
+
+    viewData.forEach((info) => {
+
+        // console.log(data.category);
+        const div = document.createElement('div');
+        div.innerHTML = `<button onclick="loadInformation('${info.category_id}')" class="btn capitalize hover:bg-red-500 hover:text-white text-black">${info.category}</button>
+        `
+
+        buttonContainer.appendChild(div);
+    })
+
+
+};
+
+
+
+
+const loadInformation = async (categoryAllId) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryAllId}`);
+    const data = await res.json();
+
+if(data.data.length==0){
+    drawingBtn(true)
+}
+else{
+    drawingBtn(false)
+}
+
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.innerHTML = "";
+    let details = data.data;
+
+
+
+
+    details.forEach((all) => {
+
+        let values = all.others.posted_date
+        const totalSec = values;
+        
+        const remainingMin = totalSec % 60;
+        const totalMin = (totalSec - remainingMin) / 60;
+
+        const remainingHour = totalMin % 60;
+        const totalhour =(totalMin - remainingHour) / 60;
+
+        const time = values.length !== 0 ? `${totalhour}hrs ${totalMin}min ago` : "";
+
+        // let time;
+        // if(values.length != 0){
+        //     time = `${totalhour}hrs ${totalMin}min ago : ""`
+        // }
+
+
+
+        const div = document.createElement('div');
+        div.classList=`card card-compact mt-5 h-[300px] mx-auto
+         max-w-[300px]`
+        div.innerHTML = `
+        <figure class="h-[210px]"><img src="${all.thumbnail}" />
+        <p class="posted-date absolute bottom-36 right-2 text-white">${time}</p>
+        </figure>
+        <div class=" card-body flex flex-row">
+            <img class="h-[40px] w-[40px] rounded-full" src="${all.authors[0].profile_picture}" alt="">
+            <h2 class="card-title">${all.title}</h2>
+        </div>
+        <div class="ml-16">
+        <p class="flex"> ${all.authors[0].profile_name} <img src="${all.authors[0].verified ? './image/badge.svg' : ''}"/> </p>
+       
+        <p class="mb-5">${all.others.views} views</p>
+        </div>`
+
+
+        cardContainer.appendChild(div);
+
+
+        // console.log(details);
+
+    })
+
+};
+
+const drawingBtn=(button)=>{
+if(button){
+    const showbtn = document.getElementById('drawingBtn');
+    showbtn.classList.remove('hidden');
+}
+else{
+    const showbtn = document.getElementById('drawingBtn');
+    showbtn.classList.add('hidden');
+}
+}
+
+
+loadCategory();
+loadInformation('1000');
